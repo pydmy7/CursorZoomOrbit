f21d5d02d781e7d9b1b28ebe1892c20b5a2c2e84
pydmy7 <91300046+pydmy7@users.noreply.github.com>
2025-09-03 19:03:42 +0800

    CursorZoomOrbitCamera

--- Source/CursorZoomOrbit/CursorZoomOrbitCamera.cpp
+++ Source/CursorZoomOrbit/CursorZoomOrbitCamera.cpp
@@ -1,34 +1,190 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CursorZoomOrbitCamera.h"
 
-// Sets default values
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/SceneComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
+#include "GameFramework/PlayerController.h"
+#include "Engine/LocalPlayer.h"
+
 ACursorZoomOrbitCamera::ACursorZoomOrbitCamera()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    PrimaryActorTick.bCanEverTick = false;
 
+    Pivot = CreateDefaultSubobject<USceneComponent>(TEXT("Pivot"));
+
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+    SpringArm->SetupAttachment(Pivot);
+    SpringArm->TargetArmLength = 800.f;
+    SpringArm->bDoCollisionTest = false;
+    SpringArm->bUsePawnControlRotation = false;
+    SpringArm->bInheritPitch = false;
+    SpringArm->bInheritYaw = false;
+    SpringArm->bInheritRoll = false;
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
+
+    RootComponent = Pivot;
 }
 
-// Called when the game starts or when spawned
 void ACursorZoomOrbitCamera::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    DefaultArmLength = SpringArm->TargetArmLength;
+    DefaultArmRot = SpringArm->GetRelativeRotation();
+    DefaultPivotLoc = Pivot->GetComponentLocation();
+
+    if (APlayerController* PC = Cast<APlayerController>(GetController()))
+    {
+        PC->bShowMouseCursor = true;
+        FInputModeGameOnly Mode;
+        Mode.SetConsumeCaptureMouseDown(false);
+        PC->SetInputMode(Mode);
+
+        if (ULocalPlayer* LP = PC->GetLocalPlayer())
+        {
+            if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LP))
+            {
+                if (IMC_Camera)
+                {
+                    Subsystem->AddMappingContext(IMC_Camera, 0);
+                }
+            }
+        }
+    }
 }
 
-// Called every frame
-void ACursorZoomOrbitCamera::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
 void ACursorZoomOrbitCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        if (IA_LMB)
+        {
+            EIC->BindAction(IA_LMB, ETriggerEvent::Started,   this, &ACursorZoomOrbitCamera::OnLMBStarted);
+            EIC->BindAction(IA_LMB, ETriggerEvent::Completed, this, &ACursorZoomOrbitCamera::OnLMBCompleted);
+            EIC->BindAction(IA_LMB, ETriggerEvent::Canceled,  this, &ACursorZoomOrbitCamera::OnLMBCompleted);
+        }
+        if (IA_MMB)
+        {
+            EIC->BindAction(IA_MMB, ETriggerEvent::Started,   this, &ACursorZoomOrbitCamera::OnMMBStarted);
+            EIC->BindAction(IA_MMB, ETriggerEvent::Completed, this, &ACursorZoomOrbitCamera::OnMMBCompleted);
+            EIC->BindAction(IA_MMB, ETriggerEvent::Canceled,  this, &ACursorZoomOrbitCamera::OnMMBCompleted);
+        }
+        if (IA_MouseX)
+        {
+            EIC->BindAction(IA_MouseX, ETriggerEvent::Triggered, this, &ACursorZoomOrbitCamera::OnMouseX);
+        }
+        if (IA_MouseY)
+        {
+            EIC->BindAction(IA_MouseY, ETriggerEvent::Triggered, this, &ACursorZoomOrbitCamera::OnMouseY);
+        }
+        if (IA_MouseWheel)
+        {
+            EIC->BindAction(IA_MouseWheel, ETriggerEvent::Triggered, this, &ACursorZoomOrbitCamera::OnMouseWheel);
+        }
+        if (IA_Reset)
+        {
+            EIC->BindAction(IA_Reset, ETriggerEvent::Triggered, this, &ACursorZoomOrbitCamera::OnReset);
+        }
+    }
 }
 
+void ACursorZoomOrbitCamera::OnLMBStarted(const FInputActionValue& /*Value*/)
+{
+    bOrbiting = true;
+}
+void ACursorZoomOrbitCamera::OnLMBCompleted(const FInputActionValue& /*Value*/)
+{
+    bOrbiting = false;
+}
+
+void ACursorZoomOrbitCamera::OnMMBStarted(const FInputActionValue& /*Value*/)
+{
+    bPanning = true;
+}
+void ACursorZoomOrbitCamera::OnMMBCompleted(const FInputActionValue& /*Value*/)
+{
+    bPanning = false;
+}
+
+void ACursorZoomOrbitCamera::OnMouseX(const FInputActionValue& Value)
+{
+    const float DX = Value.Get<float>();
+    if (bOrbiting)
+    {
+        ApplyOrbit(DX, 0.f);
+    }
+    else if (bPanning)
+    {
+        ApplyPan(DX, 0.f);
+    }
+}
+
+void ACursorZoomOrbitCamera::OnMouseY(const FInputActionValue& Value)
+{
+    const float DY = Value.Get<float>();
+    if (bOrbiting)
+    {
+        ApplyOrbit(0.f, DY);
+    }
+    else if (bPanning)
+    {
+        ApplyPan(0.f, DY);
+    }
+}
+
+void ACursorZoomOrbitCamera::OnMouseWheel(const FInputActionValue& Value)
+{
+    float MinZoom = 150.f;
+    float MaxZoom = 6000.f;
+    float ZoomSpeed = 200.f;
+
+    const float W = Value.Get<float>();
+    float NewLen = SpringArm->TargetArmLength - W * ZoomSpeed;
+    NewLen = FMath::Clamp(NewLen, MinZoom, MaxZoom);
+    SpringArm->TargetArmLength = NewLen;
+}
+
+void ACursorZoomOrbitCamera::OnReset(const FInputActionValue& /*Value*/)
+{
+    SpringArm->TargetArmLength = DefaultArmLength;
+    SpringArm->SetRelativeRotation(DefaultArmRot);
+    Pivot->SetWorldLocation(DefaultPivotLoc);
+}
+
+void ACursorZoomOrbitCamera::ApplyOrbit(float DeltaX, float DeltaY)
+{
+    float OrbitSpeed = 1.5f;
+
+    constexpr float YSign = -1.f;
+    const float s = OrbitSpeed * 3.0f;
+
+    const FQuat q = SpringArm->GetComponentQuat();
+    const FVector camRight = q.GetRightVector();
+    const FVector camUp = q.GetUpVector();
+
+    const FQuat dq = FQuat(camUp, FMath::DegreesToRadians(DeltaX * s)) * FQuat(camRight, FMath::DegreesToRadians(DeltaY * s * YSign));
+
+    SpringArm->SetRelativeRotation((dq * q).GetNormalized());
+}
+
+void ACursorZoomOrbitCamera::ApplyPan(float DeltaX, float DeltaY)
+{
+    float PanSpeed = 0.8f;
+    const FVector Right = Camera->GetRightVector();
+    const FVector Up = Camera->GetUpVector();
+    const float PanScale = PanSpeed * (SpringArm->TargetArmLength / 800.f);
+
+    const float PanYSign = -1.f;
+    const FVector Offset = (-Right * DeltaX + Up * (PanYSign * DeltaY)) * PanScale;
+
+    Pivot->AddWorldOffset(Offset, false);
+}
--- Source/CursorZoomOrbit/CursorZoomOrbitCamera.h
+++ Source/CursorZoomOrbit/CursorZoomOrbitCamera.h
@@ -6,24 +6,71 @@
 #include "GameFramework/Pawn.h"
 #include "CursorZoomOrbitCamera.generated.h"
 
+class USceneComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class CURSORZOOMORBIT_API ACursorZoomOrbitCamera : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ACursorZoomOrbitCamera();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputMappingContext> IMC_Camera = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_MouseX = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_MouseY = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_MouseWheel = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_LMB = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_MMB = nullptr;
+
+    UPROPERTY(EditDefaultsOnly, Category="Input")
+    TObjectPtr<UInputAction> IA_Reset = nullptr;
+
+private:
+    UPROPERTY()
+    TObjectPtr<USceneComponent> Pivot;
+    UPROPERTY()
+    TObjectPtr<USpringArmComponent> SpringArm;
+    UPROPERTY()
+    TObjectPtr<UCameraComponent> Camera;
+
+    float DefaultArmLength = 800.f;
+    FRotator DefaultArmRot = FRotator::ZeroRotator;
+    FVector DefaultPivotLoc = FVector::ZeroVector;
+
+    bool bOrbiting = false;
+    bool bPanning = false;
+
+    void OnLMBStarted(const FInputActionValue& Value);
+    void OnLMBCompleted(const FInputActionValue& Value);
+    void OnMMBStarted(const FInputActionValue& Value);
+    void OnMMBCompleted(const FInputActionValue& Value);
+
+    void OnMouseX(const FInputActionValue& Value);
+    void OnMouseY(const FInputActionValue& Value);
+    void OnMouseWheel(const FInputActionValue& Value);
+    void OnReset(const FInputActionValue& Value);
+
+    void ApplyOrbit(float DeltaX, float DeltaY);
+    void ApplyPan(float DeltaX, float DeltaY);
 };
