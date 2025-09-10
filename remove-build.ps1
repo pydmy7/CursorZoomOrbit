fa18dc1065d14a0be01f8b7fcdcba08dc705d422
wangyao <1822364545@qq.com>
2025-09-10 22:18:36 +0800

    Fix remove binary script

--- remove-build.ps1
+++ remove-build.ps1
@@ -2,9 +2,15 @@ Remove-Item -Recurse -Force -Path .cache
 Remove-Item -Recurse -Force -Path .idea
 Remove-Item -Recurse -Force -Path .vs
 Remove-Item -Recurse -Force -Path .vsconfig
-Remove-Item -Recurse -Force -Path Binaries
-Remove-Item -Recurse -Force -Path Intermediate
-Remove-Item -Recurse -Force -Path DerivedDataCache
-Remove-Item -Recurse -Force -Path Saved
 Remove-Item -Recurse -Force -Path compile_commands.json
 Remove-Item -Recurse -Force -Path CursorZoomOrbit.sln
+Remove-Item -Recurse -Force -Path DerivedDataCache
+Remove-Item -Recurse -Force -Path Saved
+
+# Remove-Item -Recurse -Force -Path Binaries
+# Remove-Item -Recurse -Force -Path Intermediate
+# Remove-Item -Recurse -Force -Path .\Plugins\UnrealImGui\Binaries\
+# Remove-Item -Recurse -Force -Path .\Plugins\UnrealImGui\Intermediate\
+
+$paths = @(".\Binaries",".\Intermediate",".\Plugins\*\Binaries",".\Plugins\*\Intermediate")
+Remove-Item -Recurse -Force -Path $paths -ErrorAction SilentlyContinue
