455a38c702085c15843ac552066497be9f2d7d82
wangyao <1822364545@qq.com>
2025-09-10 22:19:23 +0800

    clear comment

--- remove-build.ps1
+++ remove-build.ps1
@@ -7,10 +7,5 @@ Remove-Item -Recurse -Force -Path CursorZoomOrbit.sln
 Remove-Item -Recurse -Force -Path DerivedDataCache
 Remove-Item -Recurse -Force -Path Saved
 
-# Remove-Item -Recurse -Force -Path Binaries
-# Remove-Item -Recurse -Force -Path Intermediate
-# Remove-Item -Recurse -Force -Path .\Plugins\UnrealImGui\Binaries\
-# Remove-Item -Recurse -Force -Path .\Plugins\UnrealImGui\Intermediate\
-
 $paths = @(".\Binaries",".\Intermediate",".\Plugins\*\Binaries",".\Plugins\*\Intermediate")
 Remove-Item -Recurse -Force -Path $paths -ErrorAction SilentlyContinue
