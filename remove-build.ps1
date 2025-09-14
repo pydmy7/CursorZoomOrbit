ae2f53336e602e9ecc86b1608dbdb7845600cfb0
wangyao <1822364545@qq.com>
2025-09-14 13:43:30 +0800

    script

--- remove-build.ps1
+++ remove-build.ps1
@@ -2,6 +2,7 @@ Remove-Item -Recurse -Force -Path .cache
 Remove-Item -Recurse -Force -Path .idea
 Remove-Item -Recurse -Force -Path .vs
 Remove-Item -Recurse -Force -Path .vsconfig
+Remove-Item -Recurse -Force -Path *.uproject.DotSettings.user
 Remove-Item -Recurse -Force -Path compile_commands.json
 Remove-Item -Recurse -Force -Path CursorZoomOrbit.sln
 Remove-Item -Recurse -Force -Path DerivedDataCache
